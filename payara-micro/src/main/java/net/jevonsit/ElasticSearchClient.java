5b22573fd9cc257405339dd35c72ee0d1c088064
Clive Jevons <clive@jevons-it.net>
2016-06-12 12:24:42 +0200

    Filled in mqtt and elasticsearch functionality for payara micro app.

--- payara-micro/src/main/java/net/jevonsit/ElasticSearchClient.java
+++ payara-micro/src/main/java/net/jevonsit/ElasticSearchClient.java
@@ -1,17 +1,55 @@
 package net.jevonsit;
 
-import javax.ejb.Stateless;
-
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Stateless
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Asynchronous;
+import javax.ejb.Singleton;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.UUID;
+
+@Singleton
 public class ElasticSearchClient {
 
     private static final Logger LOG = LoggerFactory.getLogger(ElasticSearchClient.class);
 
+    TransportClient transportClient;
+
+    @PostConstruct
+    public void intialise() {
+        try {
+            transportClient = new TransportClient.Builder().settings(
+                Settings.settingsBuilder()
+                        .put("client.transport.sniff", true)
+                        .put("cluster.name", "elasticsearch")
+                        .build())
+                                                           .build()
+            .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("elasticsearch"), 9300));
+        } catch (UnknownHostException e) {
+            LOG.error("Unable to initialise ES transport client.", e);
+        }
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        if (transportClient != null) {
+            transportClient.close();
+        }
+    }
+
+    @Asynchronous
     public void publish(String message) {
         LOG.info("Publishing message: " + message);
+        IndexRequest indexRequest = new IndexRequest("clientevents", "clientevent", UUID.randomUUID().toString());
+        indexRequest.source("the_message", message);
+        transportClient.index(indexRequest);
     }
 
 }
--- payara-micro/src/main/java/net/jevonsit/MqttClient.java
+++ payara-micro/src/main/java/net/jevonsit/MqttClient.java
@@ -1,9 +1,21 @@
 package net.jevonsit;
 
-import javax.ejb.Stateless;
+import org.eclipse.paho.client.mqttv3.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Stateless
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import java.util.UUID;
+
+import static java.lang.String.format;
+
+@Singleton
 public class MqttClient {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MqttClient.class);
+    private MqttAsyncClient mqttClient;
 
     public static interface MessageCallback {
         void received(String message);
@@ -13,6 +25,68 @@ public class MqttClient {
 
     public void setCallback(MessageCallback messageCallback) {
         this.messageCallback = messageCallback;
+    }
+
+    @PostConstruct
+    public void initialise() {
+        try {
+            mqttClient = new MqttAsyncClient("tcp://mqttbroker:1883",
+                "payaramicro:" + UUID.randomUUID().toString().substring(0, 5));
+            mqttClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    LOG.warn("Connection lost.", cause);
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    LOG.debug(format("Received message on topic '%s':%n\t%s", topic, message));
+                    if (messageCallback != null) {
+                        messageCallback.received(new String(message.getPayload()));
+                    }
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    LOG.debug("Delivery complete: " + token);
+                }
+            });
+            MqttConnectOptions connectOptions = new MqttConnectOptions();
+            connectOptions.setCleanSession(true);
+            mqttClient.connect(connectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    try {
+                        mqttClient.subscribe("test/topic", 1);
+                    } catch (MqttException e) {
+                        LOG.error("Unable to subscribe.", e);
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    LOG.error("Unable to connect to mqtt broker.", exception);
+                }
+            });
+        } catch (MqttException e) {
+            LOG.error("Unable to initialise mqtt async client.", e);
+        }
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        if (mqttClient != null) {
+            try {
+                mqttClient.disconnect();
+            } catch (MqttException e) {
+                LOG.error("Problem disconnecting mqtt async client.", e);
+            }
+            try {
+                mqttClient.close();
+            } catch (MqttException e) {
+                LOG.error("Problem closing mqtt async client.", e);
+            }
+        }
     }
 
 }
